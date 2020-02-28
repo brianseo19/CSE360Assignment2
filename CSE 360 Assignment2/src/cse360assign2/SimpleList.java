a57291775f27c2d09f80d8b92dad20c31d6fd6e3
brianseo <brianseo@zoey.3520.dhcp.asu.edu>
2020-02-27 18:17:22 -0700

    Final Version

--- CSE 360 Assignment2/src/cse360assign2/SimpleList.java
+++ CSE 360 Assignment2/src/cse360assign2/SimpleList.java
@@ -194,23 +194,4 @@ public class SimpleList
 	{
 		return list.length-count;
 	}
-	public static void main(String[] args)
-	{
-		SimpleList test = new SimpleList();
-		test.add(1);
-		test.add(2);
-		test.add(3);
-		test.add(4);
-		test.add(5);
-		test.add(6);
-		test.add(7);
-		test.add(8);
-		test.append(4);
-		System.out.println(test.toString());
-		System.out.println(test.first());
-		System.out.println(test.last());
-		System.out.println(test.size());
-		System.out.println(test.list.length);
-	
-	}
 }
